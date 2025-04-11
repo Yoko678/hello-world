f2a3c162013eeadc0e17d5a31e60a9e386f07be6
Yoko678 <yokojournal.co@gmail.com>
2025-04-11 15:50:44 +0700

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,16 @@
+public class xYzAbC123 {
+public static void s0rTtHeArRaY(int[] hELp){
+for(int i=0;i<hELp.length-1;i++){
+  for(int j=0;j<hELp.length-1;j++)
+{
+if(hELp[j]<hELp[j+1]){ // Bug: should be > for ascending
+int tmp=hELp[j];hELp[j]=hELp[j+1];hELp[j+1]=tmp;
+}}}}
+
+public static void maIn(String[] argz){
+int[] bOoM={5,3,8,4,2};
+s0rTtHeArRaY(bOoM);
+for(int X=0;X<=bOoM.length;X++) // Bug: should be < instead of <=
+System.out.println(bOoM[X]); // Will cause ArrayIndexOutOfBoundsException
+}
+}
